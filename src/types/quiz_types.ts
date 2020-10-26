06f428beba21546b890ad25c35523ce8bdad5c77
Jameel Ahmad <66188646+Jameelmanzoor@users.noreply.github.com>
2020-10-26 11:45:03 +0500

    Types are created to use the

---
+++ src/types/quiz_types.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export type QuizQuestionsType = {
+  category: string,
+  correct_answer: string,
+  difficulty: string,
+​  incorrect_answers: string[],
+​  question: string,
+​  type: string
+}
+
+export type QuizType = {
+  question: string
+  answer: string,
+  options: string[]
+}
+
+export type QuestionPropsType = {
+  question: string, 
+  options: string[]
+  callback: (e: React.FormEvent<EventTarget>) => void
+}
